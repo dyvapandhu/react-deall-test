5aba25a25926eecfd39bd845ab512e625561e01f
dyvapandhu <dyvapandhu989@gmail.com>
2022-09-06 13:34:22 +0700

    [ADD] Skeleton loading

--- src/components/book/BookCard.js
+++ src/components/book/BookCard.js
@@ -1,15 +1,21 @@
 function BookCard(props) {
+  function showDetail() {
+    props.showDetail();
+  }
   return (
-    <div className="items-center bg-white rounded-lg border cursor-pointer shadow-md mb-4">
+    <div
+      className="items-center bg-white rounded-lg border cursor-pointer shadow-md mb-4"
+      onClick={showDetail}
+    >
       <div className="flex flex-col-3">
         <img className="rounded-t-lg" src={props.data.cover_url} alt="" />
       </div>
       <div className="flex flex-col">
-        <p className="px-4 mt-2 mb-2 text-base font-bold tracking-tight text-gray-900">
-          {props.data.title}
+        <p className="px-4 mt-2 mb-2 text-small font-bold tracking-tight text-gray-900">
+          {props.data.authors[0]}
         </p>
         <p className="px-4 mb-3 text-xs text-gray-700 dark:text-gray-400">
-          {props.data.description}
+          {props.data.category_name}
         </p>
       </div>
     </div>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,19 @@
 import BookCard from "../components/book/BookCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import BookSkeletonCard from "../components/book/BookSkeletonCard";
+// import BookModal from "../components/book/BookModal";
 
 function DashboardPage() {
+  // const [isModalOpen, setIsModalOpen] = useState([]);
   const [books, setBooks] = useState([]);
   const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("/fee-assessment-books?categoryId=1&page=0&size=10")
-      .then((res) => {
-        setBooks(res.data);
-      });
-  }, []);
+  const [categoryId, setCategoryId] = useState(null);
+  const [categoryName, setCategoryName] = useState(null);
+  const [page, setPage] = useState(0);
+  const [size] = useState(10);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     axios.get("/fee-assessment-categories").then((res) => {
@@ -20,24 +21,101 @@ function DashboardPage() {
     });
   }, []);
 
+  useEffect(() => {
+    if (categoryId) {
+      setIsLoading(true);
+      axios
+        .get(
+          `/fee-assessment-books?categoryId=${categoryId}&page=${page}&size=${size}`
+        )
+        .then((res) => {
+          setBooks((prevBooks, index) => [
+            ...prevBooks.map((book) => {
+              book.order = prevBooks.length + index +1
+              book.category_name = categoryName;
+              return book;
+            }),
+            ...res.data.map((book) => {
+              book.category_name = categoryName;
+              return book;
+            }),
+          ]);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log("err", err);
+          setIsLastPage(true);
+          setIsLoading(false);
+        });
+    }
+  }, [categoryId, page, size, categoryName]);
+
+  function handleSelectCategory(category) {
+    setCategoryName(category.name);
+    setIsLastPage(false);
+    setPage(0);
+    setBooks([]);
+    setCategoryId(category.id);
+  }
+
+  function showBookDetail() {
+    console.log("selected");
+    // setIsModalOpen(true);
+  }
+
+  function loadMore() {
+    setPage((prevPage) => prevPage + 1);
+  }
+
   return (
     <div className="container mx-auto">
-      {categories.map((category) => {
-        return (
-          <button
-            key={category.id}
-            type="button"
-            className="m-1 text-gray-900 py-2 px-3 text-xs font-medium hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
-          >
-            {category.name}
-          </button>
-        );
-      })}
-      <div className="mt-3 grid grid-cols-2 gap-6 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
-        {books.map((book) => {
-          return <BookCard key={book.id} data={book} />;
+      <div className="sticky top-0">
+        {categories.map((category) => {
+          return (
+            <button
+              key={category.id}
+              type="button"
+              className="m-1 text-gray-900 py-2 px-3 text-xs font-medium hover:text-white bg-white hover:bg-gray-900 ring-1 focus:outline-none font-medium rounded-lg text-center"
+              onClick={() => handleSelectCategory(category)}
+            >
+              {category.name}
+            </button>
+          );
         })}
       </div>
+      <div className="mt-3 grid grid-cols-2 gap-6 xs:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6">
+        {books.map((book) => {
+          return (
+            <BookCard
+              key={book.id}
+              data={book}
+              showDetail={showBookDetail}
+            />
+          );
+        })}
+        {isLoading ? (
+          <>
+            <BookSkeletonCard />
+            <BookSkeletonCard />
+            <BookSkeletonCard />
+            <BookSkeletonCard />
+            <BookSkeletonCard />
+            <BookSkeletonCard />
+          </>
+        ) : null}
+      </div>
+      {categoryId && books.length > 0 && !isLastPage ? (
+        <div className="text-center">
+          <button
+            type="button"
+            className="m-1 text-gray-900 py-2 px-3 text-xs font-medium hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-1 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            onClick={loadMore}
+          >
+            Load More
+          </button>
+        </div>
+      ) : null}
+      {/* <BookModal isModalOpen={isModalOpen} closeModal={closeModal} /> */}
     </div>
   );
 }
